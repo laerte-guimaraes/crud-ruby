e346f2f24509dda4996537c42a0101290c70c8b2
laerte-guimaraes <laerte.guimaraes@vindi.com.br>
2019-02-05 14:13:00 -0200

    Ajusta fluxo

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190201143051) do
+ActiveRecord::Schema.define(version: 20190201184409) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
     t.boolean  "status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_tasks_on_category_id"
   end
 
 end
